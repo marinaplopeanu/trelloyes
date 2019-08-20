bc063e981035233484b8dc23cfd877ccc4a356cd
Marina Plopeanu <Marina.Plopeanu@fwwebbcorp.fwwebb.com>
2019-08-20 13:06:38 -0400

    added test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './store.js';
+import React, { Component } from 'react';
+import List from './List'
+import './App.css';
 
-function App() {
-  return (
-   <main className="App">
-     {/*content goes here */}
-   </main>
-  );
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+   
+  };
+
+  render() {
+     console.log(this.props);
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+import List from './List';
+
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button type='button'>Delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
+}
